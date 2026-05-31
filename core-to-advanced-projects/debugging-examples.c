374c9b6c0bc307792d96ecae6d968875a0096907
Miss Chandan Jain <chandanjain326@gmail.com>
2026-05-31 21:08:38 +0530

    Create debugging-examples.c

---
+++ core-to-advanced-projects/debugging-examples.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 10;
+    int b = 5;
+
+    // Debugging के बाद सही Addition
+    int sum = a + b;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
