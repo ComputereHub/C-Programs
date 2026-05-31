6849345959a93a12aa81f8f46022f094e8651814
Miss Chandan Jain <chandanjain326@gmail.com>
2026-05-31 16:43:30 +0530

    Create sorting-array.c

---
+++ array-programs/sorting-array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5], temp;
+
+    // Array input करें
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Ascending order में sorting करें
+    for(int i = 0; i < 4; i++) {
+        for(int j = i + 1; j < 5; j++) {
+            if(arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf("Sorted Array: ");
+
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
